bdf1570810819ec8d91395a181110622ca580d61
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-11-12 17:24:33 -0800

    Formatted file.

--- PipelineBuiltInBasic/Program.cs
+++ PipelineBuiltInBasic/Program.cs
@@ -1,25 +1,29 @@
+// <copyright file="Program.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		var app = builder.Build();
 
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler();
-        }
-        else
-        {
-            app.UseDeveloperExceptionPage();
-        }
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler();
+		}
+		else
+		{
+			app.UseDeveloperExceptionPage();
+		}
 
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseWelcomePage("/");
-        
-        app.MapGet("/hello", () => "Hello World!");
+		app.UseStaticFiles();
+		app.UseRouting();
+		app.UseWelcomePage("/");
 
-        app.Run();
-    }
+		app.MapGet("/hello", () => "Hello World!");
+
+		app.Run();
+	}
 }
