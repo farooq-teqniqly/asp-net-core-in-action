5767e92049d14eb58121b52dab03a6245e7b948f
farooq-teqniqly <farooq@teqniqly.com>
2023-11-13 20:46:23 -0800

    Post endpoint and tests.

---
+++ FruitApi.IntegrationTests/ApiTestClient.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ApiTestClient.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace FruitApi.IntegrationTests
+{
+	using System.Text;
+	using System.Text.Json;
+
+	public class ApiTestClient
+	{
+		private readonly HttpClient client;
+
+		public ApiTestClient(HttpClient httpClient) => client = httpClient;
+
+		public async Task<HttpResponseMessage> PostAsync(string endpoint, object data)
+		{
+			var json = JsonSerializer.Serialize(data);
+			var content = new StringContent(json, Encoding.UTF8, "application/json");
+			return await client.PostAsync(endpoint, content);
+		}
+
+		public async Task<HttpResponseMessage> GetAsync(string endpoint) => await client.GetAsync(endpoint);
+
+		public void Dispose() => client.Dispose();
+	}
+}
---
+++ FruitApi.IntegrationTests/PostEndpointIntegrationTests.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="PostEndpointIntegrationTests.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace FruitApi.IntegrationTests
+{
+	using FluentAssertions;
+	using System;
+	using System.Net;
+	using System.Threading.Tasks;
+
+	public class PostEndpointIntegrationTests : IClassFixture<CustomWebApplicationFactory>, IDisposable
+	{
+		private readonly ApiTestClient client;
+
+		public PostEndpointIntegrationTests(CustomWebApplicationFactory fixture)
+			=> client = new ApiTestClient(fixture.CreateClient());
+
+		[Fact]
+		public async Task Post_When_Successful_Returns_Created_Status_Code()
+		{
+			var response = await client.PostAsync(
+				"/fruit",
+				new { name = "Banana", Stock = 10 });
+
+			response.StatusCode.Should().Be(HttpStatusCode.Created);
+		}
+
+		[Fact]
+		public async Task Post_When_Successful_Returns_Id_In_Location_Header()
+		{
+			var response = await client.PostAsync(
+				"/fruit",
+				new { name = "Banana", Stock = 10 });
+
+			var location = response.Headers.Location;
+			location.Should().NotBeNull();
+			location.ToString().Split("/")[1].Length.Should().BeGreaterThan(1);
+		}
+
+		public void Dispose() => client.Dispose();
+	}
+}
